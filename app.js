6d4d32d8785615f2eedd2059d7890228784fd132
shogen0510 <136776075+shogen0510@users.noreply.github.com>
2023-06-22 19:49:25 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -104,16 +104,12 @@ document.addEventListener("DOMContentLoaded", function() {
         } else if(type === 'DB') {
             headers = ["防具", "防具分類1", "強化Lv", "必要素材", "必要数量"];
         }
-        if (!table) {
-            console.error("Unable to find an element with the id '" + tableId + "' in the DOM");
-            return;
-        }
-
+    
         // Clear out any existing rows
         while (table.firstChild) {
             table.removeChild(table.firstChild);
         }
-
+    
         // Create table headers
         let thead = document.createElement("thead");
         let headerRow = document.createElement("tr");
@@ -124,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
         thead.appendChild(headerRow);
         table.appendChild(thead);
-
+    
         // Add table rows
         data.sort((a, b) => a['No'] - b['No']).forEach(row => {
             let tr = document.createElement("tr");
@@ -149,4 +145,76 @@ document.addEventListener("DOMContentLoaded", function() {
             table.appendChild(tr);
         });
     }
+
+    function searchDB(keyword) {
+        db.collection("DB").get().then((querySnapshot) => {
+            let searchData = [];
+            querySnapshot.forEach((doc) => {
+                let data = doc.data();
+                if(data.強化済みフラグ === 0 && (data.防具.includes(keyword) || data.防具分類1.includes(keyword) || data.強化Lv.includes(keyword) || data.必要素材.includes(keyword))){
+                    data.id = doc.id;
+                    searchData.push(data);
+                }
+            });
+            searchData.sort((a, b) => a['No.'] - b['No.']); // Sort searchData based on 'No'
+            let quantities = aggregateMaterialQuantities(searchData);
+            createQuantityTable(quantities, 'quantity-table');
+            createTable(searchData, 'DB', 'search-table');
+        });
+    }
+    
+    function saveStatus() {
+        let checkboxes = document.querySelectorAll("input[type='checkbox']");
+        checkboxes.forEach(checkbox => {
+            let level = checkbox.id;
+            let checked = checkbox.checked ? 1 : 0;  // if checked, set 1, else set 0
+    
+            // Update '強化済みフラグ' in all matching STATUS documents
+            db.collection("STATUS").where('防具強化Lv', '==', level).get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    db.collection("STATUS").doc(doc.id).update({
+                        '強化済みフラグ': checked
+                    });
+                });
+            }).catch(err => console.log(err));
+    
+            // Update '強化済みフラグ' in all matching DB documents
+            db.collection("DB").where('防具強化Lv', '==', level).get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    db.collection("DB").doc(doc.id).update({
+                        '強化済みフラグ': checked
+                    });
+                });
+            }).catch(err => console.log(err));
+        });
+        alert("Saved!");
+    }
+
+    function clearStatus() {
+        let checkboxes = document.querySelectorAll("input[type='checkbox']");
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+        alert("Cleared!");
+    }
+
+    let searchBtn = document.getElementById("searchBtn");
+    let saveBtn = document.getElementById("saveBtn");
+    let clearBtn = document.getElementById("clearBtn");
+
+    searchBtn.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the form from being submitted normally
+        let searchKeyword = document.getElementById("search").value; // Get the search input value
+        searchDB(searchKeyword);
+    });   
+
+    saveBtn.addEventListener("click", function() {
+        // When STATUS SAVE button is clicked, execute the saveStatus function
+        saveStatus();
+    });
+
+    clearBtn.addEventListener("click", function() {
+        // When STATUS CLEAR button is clicked, execute the clearStatus function
+        clearStatus();
+    });
 });

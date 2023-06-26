cef5338ccfeff14d8dcfc421f9a9bce35fdfd0fb
shogen0510 <136776075+shogen0510@users.noreply.github.com>
2023-06-27 01:00:55 +0900

    Add files via upload

--- top-app.js
+++ top-app.js
@@ -2,10 +2,11 @@
 var uiConfig = {
     signInSuccessUrl: 'search.html', // ログイン成功後にリダイレクトするURL
     signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID
-    ],
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+  ],
 };
 
 // FirebaseUIのインスタンスを初期化
@@ -13,15 +14,15 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 // ユーザーのログイン状態の監視
 firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // ユーザーがログインしている場合
-      document.getElementById('login-page').style.display = 'none';
-    } else {
-      // ユーザーがログアウトしている場合
-      document.getElementById('login-page').style.display = 'block';
-      // Firebase UIのウィジェットを開始
-      ui.start('#firebaseui-auth-container', uiConfig);
-    }
+  if (user) {
+    // ユーザーがログインしている場合
+    document.getElementById('login-page').style.display = 'none';
+  } else {
+    // ユーザーがログアウトしている場合
+    document.getElementById('login-page').style.display = 'block';
+    // Firebase UIのウィジェットを開始
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
 }, function(error) {
-    console.log(error);
+  console.log(error);
 });

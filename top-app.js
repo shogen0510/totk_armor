9ff255b2277449a6763f2f21f526578276e49a28
shogen0510 <136776075+shogen0510@users.noreply.github.com>
2023-06-26 19:01:06 +0900

    Add files via upload

--- top-app.js
+++ top-app.js
@@ -1,46 +1,27 @@
-// Google認証プロバイダオブジェクトのインスタンス化
-var googleProvider = new firebase.auth.GoogleAuthProvider();
-
-// Facebook認証プロバイダオブジェクトのインスタンス化
-var facebookProvider = new firebase.auth.FacebookAuthProvider();
-
-// Googleサインインボタンにクリックイベントリスナを追加
-document.getElementById('google-signin').addEventListener('click', function(event) {
-    // デフォルトのリンククリックの動作を抑制
-    event.preventDefault();
-    // Googleサインインのポップアップウィンドウを表示
-    firebase.auth().signInWithPopup(googleProvider).then(function(result) {
-        // ユーザーがサインインした後の処理
-        console.log('User signed in with Google');
-    }).catch(function(error) {
-        // エラーハンドリング
-        console.error('Error occurred during Google sign-in', error);
-    });
-});
-
-// Facebookサインインボタンにクリックイベントリスナを追加
-document.getElementById('facebook-signin').addEventListener('click', function(event) {
-    // デフォルトのリンククリックの動作を抑制
-    event.preventDefault();
-    // Facebookサインインのポップアップウィンドウを表示
-    firebase.auth().signInWithPopup(facebookProvider).then(function(result) {
-        // ユーザーがサインインした後の処理
-        console.log('User signed in with Facebook');
-    }).catch(function(error) {
-        // エラーハンドリング
-        console.error('Error occurred during Facebook sign-in', error);
-    });
-});
-
-// ユーザーのログイン状態の監視
-firebase.auth().onAuthStateChanged(function(user) {
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'search.html', // ログイン成功後にリダイレクトするURL
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+  };
+  
+  // FirebaseUIのインスタンスを初期化
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  
+  // ユーザーのログイン状態の監視
+  firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        // ユーザーがログインしている場合
-        document.getElementById('login-page').style.display = 'none';
-        // ログイン成功後、search.htmlページにリダイレクト
-        window.location.href = "search.html";
+      // ユーザーがログインしている場合
+      document.getElementById('login-page').style.display = 'none';
     } else {
-        // ユーザーがログアウトしている場合
-        document.getElementById('login-page').style.display = 'block';
+      // ユーザーがログアウトしている場合
+      document.getElementById('login-page').style.display = 'block';
+      // Firebase UIのウィジェットを開始
+      ui.start('#firebaseui-auth-container', uiConfig);
     }
-});
+  }, function(error) {
+    console.log(error);
+  });
+  

9dcb4a8adfb78dd64b07fbf1bd625c12aa7ac4c8
shogen0510 <136776075+shogen0510@users.noreply.github.com>
2023-07-05 06:51:09 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -196,15 +196,18 @@ document.addEventListener("DOMContentLoaded", function() {
         let statusData = JSON.parse(localStorage.getItem('STATUS'));
         let searchData = [];
     
+        // Define the fields to be included in the search
+        let searchFields = ["強化Lv", "必要素材", "防具", "防具分類1"];
+    
         // Split the keyword by space (both full-width and half-width) to get an array of keywords
         let keywords = keyword.split(/[\s\u3000]/);
     
         dbData.forEach(data => {
             // Only look at items that have not been enhanced
             if (statusData.some(status => status['防具強化Lv'] === data['防具強化Lv'] && status['強化済みフラグ'] === 0)) {
-                // Check if each keyword is included in the document
+                // Check if each keyword is included in the searchFields of the document
                 let isAllKeywordsIncluded = keywords.every(kw => {
-                    return Object.values(data).some(v => v.toString().includes(kw));
+                    return searchFields.some(field => data[field].toString().includes(kw));
                 });
         
                 // If all keywords are included, add the document to searchData

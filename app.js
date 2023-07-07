e7fb376db895b031196fbb26fe92a66a03b0b6d9
shogen0510 <136776075+shogen0510@users.noreply.github.com>
2023-07-07 20:00:13 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -220,29 +220,24 @@ document.addEventListener("DOMContentLoaded", function() {
                 let td = document.createElement("td");
                 if (header === '強化済みフラグ' && type === 'STATUS') {
                     let label = document.createElement("label");
-                    label.className = "cbx";
-                
+                    label.className = "ECM_CheckboxInput";
+
                     let checkbox = document.createElement("input");
                     checkbox.type = "checkbox";
-                    checkbox.className = "inp-cbx";
+                    checkbox.className = "ECM_CheckboxInput-Input";
                     checkbox.checked = row[header];
                     checkbox.id = row['防具強化Lv'].toString();
                     label.appendChild(checkbox);
-                
-                    let span1 = document.createElement("span");
-                    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                    svg.setAttribute("width", "12px");
-                    svg.setAttribute("height", "10px");
-                    svg.setAttribute("viewBox", "0 0 12 10");
-                    let polyline = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
-                    polyline.setAttribute("points", "1.5 6 4.5 9 10.5 1");
-                    svg.appendChild(polyline);
-                    span1.appendChild(svg);
-                    label.appendChild(span1);
-                
-                    let span2 = document.createElement("span");
-                    label.appendChild(span2);
-                
+
+                    let dummyInput = document.createElement("span");
+                    dummyInput.className = "ECM_CheckboxInput-DummyInput";
+                    label.appendChild(dummyInput);
+
+                    let labelText = document.createElement("span");
+                    labelText.className = "ECM_CheckboxInput-LabelText";
+                    labelText.textContent = row['防具強化Lv'].toString();
+                    label.appendChild(labelText);
+
                     td.appendChild(label);
                 } else if ((header === '防具' || header === '必要素材') && links[row[header]]) {
                     let link = document.createElement("a");
